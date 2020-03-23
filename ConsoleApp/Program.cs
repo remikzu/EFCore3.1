f5d2cf4aadf5bd8ba76c7fa24f21e1a61a5ba7e4
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-03-23 13:32:31 +0100

    Inserting, Updating & Selecting Queries via EF Core 3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,35 +5,86 @@ using System.Linq;
 
 namespace ConsoleApp
 {
-  internal class Program
-  {
-    private static SamuraiContext context = new SamuraiContext();
-
-    private static void Main(string[] args)
+    internal class Program
     {
-      context.Database.EnsureCreated();
-      GetSamurais("Before Add:");
-      AddSamurai();
-      GetSamurais("After Add:");
-      Console.Write("Press any key...");
-      Console.ReadKey();
-    }
+        private static SamuraiContext context = new SamuraiContext();
 
-    private static void AddSamurai()
-    {
-      var samurai = new Samurai { Name = "Sampson" };
-      context.Samurais.Add(samurai);
-      context.SaveChanges();
-    }
+        private static void Main(string[] args)
+        {
+            //InsertVariousTypes();
+            //InsertMultipleSamurais();
+            //AddSamurai();
+            //GetSamurais("After Add:");
+            GetSamuraisUsingLINQMethods();
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
 
-    private static void GetSamurais(string text)
-    {
-      var samurais = context.Samurais.ToList();
-      Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-      foreach (var samurai in samurais)
-      {
-        Console.WriteLine(samurai.Name);
-      }
+        private static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Remik" };
+            var samurai2 = new Samurai { Name = "Radek" };
+            var samurai3 = new Samurai { Name = "Tomek" };
+            var samurai4 = new Samurai { Name = "Grzegorz" };
+            var samurai5 = new Samurai { Name = "Kasia" };
+            //context.Samurais.Add(samurai);
+            //context.Samurais.Add(samurai2);
+            context.Samurais.AddRange(samurai, samurai2, samurai3, samurai4, samurai5);
+            context.SaveChanges();
+        }
+
+        private static void InsertVariousTypes()
+        {
+            var samurai = new Samurai { Name = "Kikuchio" };
+            var clan = new Clan { ClanName = "Imperial Clan" };
+            context.AddRange(samurai, clan);
+            context.SaveChanges();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamuraisUsingLINQMethods()
+        {
+            //var samuraisLINQMethod = context.Samurais.ToList();
+            var samuraisLINQMethod2 = context.Samurais
+                                             .Where(s => s.Name == "Remik")
+                                             .ToList();
+            foreach (var samurai in samuraisLINQMethod2)
+            {
+                Console.WriteLine($"{samurai.Id}: {samurai.Name}");
+            }
+            //Console.WriteLine(samuraisLINQMethod2);
+        }
+
+        private static void GetSamuraisUsingLINQQuerySyntax()
+        {
+            var samuraisLINQQuery1 = from s in context.Samurais
+                                     select s;
+            samuraisLINQQuery1.ToList();
+            var samuraisLINQQuery2 = from s in context.Samurais
+                                     where s.Name == "Remik"
+                                     select s;
+            samuraisLINQQuery2.ToList();
+        }
+
+        private static void GetSamuraiSimpler()
+        {
+            var samurais = context.Samurais.ToList();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine($"{samurai.Id}: {samurai.Name}");
+            }
+        }
     }
-  }
 }
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 using System;
 using System.Collections.Generic;
@@ -13,9 +14,21 @@ namespace SamuraiApp.Data
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information)
+                .AddConsole();
+            });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
